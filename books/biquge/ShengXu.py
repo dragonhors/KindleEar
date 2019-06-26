9404103b82b2c29a0c0f38ced8c8710c7e5e6bf0
ma xinhua <14590509@qq.com>
2019-06-26 08:11:19 +0800

    新增

---
+++ books/biquge/ShengXu.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skiinder <https://github.com/skiinder>
+from books.Novelbase import xxbiqugebase
+
+
+def getBook():
+    return ShengXu
+
+
+class ShengXu(xxbiqugebase):
+    title = u'[小说]圣墟'
+    description = u'辰东'
+    feeds = 'https://www.xbiquge6.com/74_74821/'
