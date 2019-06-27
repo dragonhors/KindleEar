33dd1f78e018e32993e2266ca80179ec1346d823
ma xinhua <14590509@qq.com>
2019-06-27 11:13:23 +0800

    1

--- books/biquge/FanRen.py
+++ books/biquge/FanRen.py
@@ -11,8 +11,6 @@ def getBook():
 class FanRen(xxbiqugebase):
     title = u'[小说]凡人修仙传仙界篇'
     __author__ = u'妄语' # 设定作者
-
-    description = u'风云再起时空穿梭，轮回逆转金仙太乙，大罗道祖三千大道，法则至尊；一个韩立叱咤仙界的故事，一个凡人小子修仙的不灭传说。'
-    coverfile = 'cv_FanRen.jpg' # 设定封面图片
-
+    description = u'风云再起时空穿梭，轮回逆转金仙太乙，大罗道祖三千大道，法则至尊。'
+    coverfile = 'cv_Fanren.jpg' # 设定封面图片
     feeds = 'https://www.xbiquge6.com/1_1203/'
