ffcbb6fac9829bc37f1f63eca7d90e5e82126bfe
ma xinhua <14590509@qq.com>
2019-06-28 13:23:03 +0800

    去封面

--- books/biquge/FanRen.py
+++ books/biquge/FanRen.py
@@ -12,5 +12,5 @@ class FanRen(xxbiqugebase):
     title = u'[小说]凡人修仙传仙界篇'
     __author__ = u'妄语' # 设定作者
     description = u'风云再起时空穿梭，轮回逆转金仙太乙，大罗道祖三千大道，法则至尊。'
-    coverfile = 'cv_Fanren.jpg' # 设定封面图片
+    #coverfile = 'cv_Fanren.jpg' # 设定封面图片
     feeds = 'https://www.xbiquge6.com/1_1203/'
