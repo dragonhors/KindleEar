667fbe9db46ff3e9ec89a9c1903c908bcf4a5010
ma xinhua <14590509@qq.com>
2019-06-29 11:37:47 +0800

    0630

--- books/FanRenXiuXian.py
+++ books/FanRenXiuXian.py
@@ -8,24 +8,26 @@ from bs4 import BeautifulSoup # 导入BeautifulSoup处理模块
 
 # 返回此脚本定义的类名
 def getBook():
-    return ChinaDaily
+    return Fanren2
 
 # 继承基类BaseFeedBook
-class ChinaDaily(BaseFeedBook):
+class Fanren2(BaseFeedBook):
     # 设定生成电子书的元数据
-    title = u'China Daily' # 设定标题
-    __author__ = u'China Daily' # 设定作者
-    description = u'Chinadaily.com.cn is the largest English portal in China. ' # 设定简介
-    language = 'en' # 设定语言
+    title = u'003.凡人修仙传仙界篇'                   # 设定订阅项目显示标题
+    __author__ = u'辰东'                  # 设定作者，目前未用到
+    description = u'来自新笔趣阁'       # 设定简介，订阅项目上显示
+    language = 'zh-cn'                    # 设定语言
 
-    coverfile = 'cv_chinadaily.jpg' # 设定封面图片
-    mastheadfile = 'mh_chinadaily.gif' # 设定标头图片
-
+    coverfile = 'cv_Fanren.jpg' # 设定封面图片
+    mastheadfile = 'mh_novelbook.gif' # 设定标头图片
+    
+    feed_encoding = 'utf-8' #添加这两行实测无作用，可能前面基类已定义了吧
+    page_encoding = 'utf-8'    
+    
     # 指定要提取的包含文章列表的主题页面链接
     # 每个主题是包含主题名和主题页面链接的元组
     feeds = [
-        (u'National affairs', 'http://www.chinadaily.com.cn/china/governmentandpolicy'),
-        (u'Society', 'http://www.chinadaily.com.cn/china/society'),
+        (u'凡人修仙传仙界篇', 'https://www.xbiquge6.com/1_1203/'),
     ]
 
     page_encoding = 'utf-8' # 设定待抓取页面的页面编码
@@ -33,12 +35,13 @@ class ChinaDaily(BaseFeedBook):
 
     # 设定内容页需要保留的标签
     keep_only_tags = [
-        dict(name='span', class_='info_l'),
-        dict(name='div', id='Content'),
+        #dict(name='div', id='info'),
+        dict(name='h1'),
+        dict(name='div',id='content'),
     ]
 
-    max_articles_per_feed =40 # 设定每个主题下要最多可抓取的文章数量
-    oldest_article =1 # 设定文章的时间范围。小于等于365则单位为天，否则单位为秒，0为不限制。
+    max_articles_per_feed = 10 # 设定每个主题下要最多可抓取的文章数量
+    oldest_article = 1 # 设定文章的时间范围。小于等于365则单位为天，否则单位为秒，0为不限制。
 
     # 提取每个主题页面下所有文章URL
     def ParseFeedUrls(self):
@@ -48,14 +51,14 @@ class ChinaDaily(BaseFeedBook):
             # 分别获取元组中主题的名称和链接
             topic, url = feed[0], feed[1]
             # 把抽取每个主题页面文章链接的任务交给自定义函数ParsePageContent()
-            self.ParsePageContent(topic, url, urls, count=0)
-        #print urls             #这句代码干什么的？调试用？
-        #exit(0)                #也是调试代码？
+            self.ParsePageContent(topic, url, urls, count=0,count2=0,ccc=0)
+        #print urls
+        #exit(0)
         # 返回提取到的所有文章列表
         return urls
 
     # 该自定义函数负责单个主题下所有文章链接的抽取，如有翻页则继续处理下一页
-    def ParsePageContent(self, topic, url, urls, count):
+    def ParsePageContent(self, topic, url, urls, count,count2,ccc):
         # 请求主题页面链接并获取其内容
         result = self.GetResponseContent(url)
         # 如果请求成功，并且页面内容不为空
@@ -63,7 +66,10 @@ class ChinaDaily(BaseFeedBook):
             # 将页面内容转换成BeatifulSoup对象
             soup = BeautifulSoup(result.content, 'lxml')
             # 找出当前页面文章列表中所有文章条目
-            items = soup.find_all(name='span', class_='tw3_01_2_t')
+            items = soup.find_all(name='dd')
+
+            for ttt in items:
+                ccc += 1
 
             # 循环处理每个文章条目
             for item in items:
@@ -71,16 +77,21 @@ class ChinaDaily(BaseFeedBook):
                 link = item.a.get('href') # 获取文章链接
                 link = BaseFeedBook.urljoin(url, link) # 合成文章链接
                 count += 1 # 统计当前已处理的文章条目
-                # 如果处理的文章条目超过了设定数量则中止抽取
-                if count > self.max_articles_per_feed:
-                    break
-                # 如果文章发布日期超出了设定范围则忽略不处理
-                if self.OutTimeRange(item):
+                # 如果处理的文章条目超过了设定数量则中止抽取，改动下面的条件限制，选择抓取方式，都屏蔽掉，则抓全部
+                count2 = count + self.max_articles_per_feed 
+                if count2 < ccc:                                                            #一、只抓最后几章
                     continue
+                    
+                #if count > self.max_articles_per_feed:                                      #二、只抓前面几章
+                #    break
+                
+                # 如果文章发布日期超出了设定范围则忽略不处理
+                #if self.OutTimeRange(item):
+                #    continue
                 # 将符合设定文章数量和时间范围的文章信息作为元组加入列表
                 urls.append((topic, title, link, None))
 
-            # 如果主题页面有下一页，且已处理的文章条目未超过设定数量，则继续抓取下一页
+            # 如果主题页面有下一页，且已处理的文章条目未超过设定数量，则继续抓取下一页，递进调用自己
             next = soup.find(name='a', string='Next')
             if next and count < self.max_articles_per_feed:
                 url = BaseFeedBook.urljoin(url, next.get('href'))
@@ -112,7 +123,7 @@ class ChinaDaily(BaseFeedBook):
 
     # 清理文章URL附带字符
     def processtitle(self, title):
-        return title.replace(u' - Chinadaily.com.cn', '')
+        return title.replace(u'-凡人修仙之仙界篇 - 新笔趣阁', '')
 
     # 在文章内容被正式处理前做一些预处理
     def preprocess(self, content):
--- books/ShengXu.py
+++ books/ShengXu.py
@@ -8,24 +8,23 @@ from bs4 import BeautifulSoup # 导入BeautifulSoup处理模块
 
 # 返回此脚本定义的类名
 def getBook():
-    return ChinaDaily
+    return ShengXu_Test
 
 # 继承基类BaseFeedBook
-class ChinaDaily(BaseFeedBook):
+class ShengXu_Test(BaseFeedBook):
     # 设定生成电子书的元数据
-    title = u'China Daily' # 设定标题
-    __author__ = u'China Daily' # 设定作者
-    description = u'Chinadaily.com.cn is the largest English portal in China. ' # 设定简介
-    language = 'en' # 设定语言
+    title = u'001.圣墟'                   # 设定订阅项目显示标题
+    __author__ = u'辰东'                  # 设定作者，目前未用到
+    description = u'来自新笔趣阁'       # 设定简介，订阅项目上显示
+    language = 'zh-cn'                    # 设定语言
 
-    coverfile = 'cv_chinadaily.jpg' # 设定封面图片
-    mastheadfile = 'mh_chinadaily.gif' # 设定标头图片
+    coverfile = 'cv_shengxu.jpg' # 设定封面图片
+    mastheadfile = 'mh_novelbook.gif' # 设定标头图片
 
     # 指定要提取的包含文章列表的主题页面链接
     # 每个主题是包含主题名和主题页面链接的元组
     feeds = [
-        (u'National affairs', 'http://www.chinadaily.com.cn/china/governmentandpolicy'),
-        (u'Society', 'http://www.chinadaily.com.cn/china/society'),
+        (u'圣墟', 'https://www.xbiquge6.com/74_74821/'),
     ]
 
     page_encoding = 'utf-8' # 设定待抓取页面的页面编码
@@ -33,12 +32,13 @@ class ChinaDaily(BaseFeedBook):
 
     # 设定内容页需要保留的标签
     keep_only_tags = [
-        dict(name='span', class_='info_l'),
-        dict(name='div', id='Content'),
+        dict(name='div', id='info'),
+        dict(name='dd'),
+        dict(name='div',id='content'),
     ]
 
-    max_articles_per_feed =40 # 设定每个主题下要最多可抓取的文章数量
-    oldest_article =1 # 设定文章的时间范围。小于等于365则单位为天，否则单位为秒，0为不限制。
+    max_articles_per_feed = 10 # 设定每个主题下要最多可抓取的文章数量
+    oldest_article = 1 # 设定文章的时间范围。小于等于365则单位为天，否则单位为秒，0为不限制。
 
     # 提取每个主题页面下所有文章URL
     def ParseFeedUrls(self):
@@ -48,14 +48,14 @@ class ChinaDaily(BaseFeedBook):
             # 分别获取元组中主题的名称和链接
             topic, url = feed[0], feed[1]
             # 把抽取每个主题页面文章链接的任务交给自定义函数ParsePageContent()
-            self.ParsePageContent(topic, url, urls, count=0)
-        #print urls             #这句代码干什么的？调试用？
-        #exit(0)                #也是调试代码？
+            self.ParsePageContent(topic, url, urls, count=0,count2=0,ccc=0)
+        #print urls
+        #exit(0)
         # 返回提取到的所有文章列表
         return urls
 
     # 该自定义函数负责单个主题下所有文章链接的抽取，如有翻页则继续处理下一页
-    def ParsePageContent(self, topic, url, urls, count):
+    def ParsePageContent(self, topic, url, urls, count,count2,ccc):
         # 请求主题页面链接并获取其内容
         result = self.GetResponseContent(url)
         # 如果请求成功，并且页面内容不为空
@@ -63,7 +63,11 @@ class ChinaDaily(BaseFeedBook):
             # 将页面内容转换成BeatifulSoup对象
             soup = BeautifulSoup(result.content, 'lxml')
             # 找出当前页面文章列表中所有文章条目
-            items = soup.find_all(name='span', class_='tw3_01_2_t')
+            items = soup.find_all(name='dd')
+
+            for ttt in items:
+                ccc += 1
+
 
             # 循环处理每个文章条目
             for item in items:
@@ -71,16 +75,21 @@ class ChinaDaily(BaseFeedBook):
                 link = item.a.get('href') # 获取文章链接
                 link = BaseFeedBook.urljoin(url, link) # 合成文章链接
                 count += 1 # 统计当前已处理的文章条目
-                # 如果处理的文章条目超过了设定数量则中止抽取
-                if count > self.max_articles_per_feed:
-                    break
-                # 如果文章发布日期超出了设定范围则忽略不处理
-                if self.OutTimeRange(item):
+                # 如果处理的文章条目超过了设定数量则中止抽取，改动下面的条件限制，选择抓取方式，都屏蔽掉，则抓全部
+                count2 = count + self.max_articles_per_feed 
+                if count2 < ccc:                                                            #一、只抓最后几章
                     continue
+                    
+                #if count > self.max_articles_per_feed:                                      #二、只抓前面几章
+                #    break
+                
+                # 如果文章发布日期超出了设定范围则忽略不处理
+                #if self.OutTimeRange(item):
+                #    continue
                 # 将符合设定文章数量和时间范围的文章信息作为元组加入列表
                 urls.append((topic, title, link, None))
 
-            # 如果主题页面有下一页，且已处理的文章条目未超过设定数量，则继续抓取下一页
+            # 如果主题页面有下一页，且已处理的文章条目未超过设定数量，则继续抓取下一页，递进调用自己
             next = soup.find(name='a', string='Next')
             if next and count < self.max_articles_per_feed:
                 url = BaseFeedBook.urljoin(url, next.get('href'))
@@ -109,38 +118,6 @@ class ChinaDaily(BaseFeedBook):
                 return True
         # 如果设定时间范围为0，文章没超出设定时间范围（或没有发布时间），则返回False
         return False
-
-    # 清理文章URL附带字符
-    def processtitle(self, title):
-        return title.replace(u' - Chinadaily.com.cn', '')
-
-    # 在文章内容被正式处理前做一些预处理
-    def preprocess(self, content):
-        # 将页面内容转换成BeatifulSoup对象
-        soup = BeautifulSoup(content, 'lxml')
-        # 调用处理内容分页的自定义函数SplitJointPagination()
-        content = self.SplitJointPagination(soup)
-        # 返回预处理完成的内容
-        return unicode(content)
-
-    # 此函数负责处理文章内容页面的翻页
-    def SplitJointPagination(self, soup):
-        # 如果文章内容有下一页则继续抓取下一页
-        next = soup.find(name='a', string='Next')
-        if next:
-            # 含文章正文的标签
-            tag = dict(name='div', id='Content')
-            # 获取下一页的内容
-            result = self.GetResponseContent(next.get('href'))
-            post = BeautifulSoup(result.content, 'lxml')
-            # 将之前的内容合并到当前页面
-            soup = BeautifulSoup(unicode(soup.find(**tag)), 'html.parser')
-            soup.contents[0].unwrap()
-            post.find(**tag).append(soup)
-            # 继续处理下一页
-            return self.SplitJointPagination(post)
-        # 如果有翻页，返回拼接的内容，否则直接返回传入的​内容
-        return soup
 
     # 此自定义函数负责请求传给它的链接并返回响应内容
     def GetResponseContent(self, url):
